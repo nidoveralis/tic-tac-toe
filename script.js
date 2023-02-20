435fc514c254dcde2f54aa2eedf5c54bb1eb561f
Nidoveralis <nidoveralis@gmail.com>
2023-02-20 16:18:58 +0400

    feat:when havnt winner

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const positions = {};
 const x=[0,1,2];
 
 function clearField() {
+  for(let key in positions) {
+    delete positions[key];
+  };
   player = 'cross';
   itemsList.forEach((item)=>{
     item.classList.remove('winner');
@@ -14,74 +17,58 @@ function clearField() {
 };
 
 function openPopup(win) {
-  const popup = confirm(`Победил ${win}. Хотите сыграть ещё раз?`);
-  if(popup) {
-    for(let key in positions) {
-      delete positions[key];
-      clearField();
+  const popup = confirm(`Победил(а) ${win}. Хотите сыграть ещё раз?`);
+  clearField();
+};
+
+function winnerShow(ar, win) {
+  setTimeout(openPopup, 500, win);
+  ar.forEach(elem=>{
+    itemsList[elem].classList.add('winner');
+  })
+};
+
+function addPosition(user) {//проверяет кто выйграл
+    if(positions[0]===user && positions[1]===user && positions[2]===user){
+      winnerShow([0,1,2], user)
+    }else if(positions[3]===user && positions[4]===user && positions[5]===user){
+     winnerShow([3,4,5], win)
+    }else if(positions[6]===user && positions[7]===user && positions[8]===user){
+      winnerShow([6,7,8], user)
+    }else if(positions[0]===user && positions[3]===user && positions[6]===user){
+      winnerShow([0,3,6], user)
+    }else if(positions[1]===user && positions[4]===user && positions[7]===user){
+      winnerShow([1,4,7], user)
+    }else if(positions[2]===user && positions[5]===user && positions[8]===user){
+      winnerShow([2,5,8], user)
+    }else if(positions[0]===user && positions[4]===user && positions[8]===user){
+      winnerShow([0,4,8], user)
+    }else if(positions[2]===user && positions[4]===user && positions[6]===user){
+      winnerShow([2,4,6], user)
+    }else {
+      if(Object.keys(positions).length == 9){
+        setTimeout(openPopup, 500, 'дружба');
+      };
     };
-  };
 };
 
-function addPosition(zz,s) {//проверяет кто выйграл
- if(positions[0]===zz && positions[1]===zz && positions[2]===zz){
-  itemsList[0].classList.add('winner');
-  itemsList[1].classList.add('winner');
-  itemsList[2].classList.add('winner');
-  setTimeout(openPopup, 1000, zz)
-  }else if(positions[3]===zz && positions[4]===zz && positions[5]===zz){
-    openPopup(zz);
-  }else if(positions[6]===zz && positions[7]===zz && positions[8]===zz){
-    openPopup(zz);
-  }else if(positions[0]===zz && positions[3]===zz && positions[6]===zz){
-    openPopup(zz);
-  }else if(positions[1]===zz && positions[4]===zz && positions[7]===zz){
-    openPopup(zz);
-  }else if(positions[2]===zz && positions[5]===zz && positions[8]===zz){
-    openPopup(zz);
-  }else if(positions[0]===zz && positions[4]===zz && positions[8]===zz){
-    openPopup(zz);
-  }else if(positions[2]===zz && positions[4]===zz && positions[6]===zz){
-    openPopup(zz);
-  }
-};
-
-function a(e, ind) {////ставит крестик или нолик
-  const s = e.target
-  
-  if(player === 'circle' && s.style.backgroundImage ==='') {
-    s.style.backgroundImage = circle;
-   // positions.circle.push(ind)
-    positions[ind]='circle'
-    //addPosition('circle',ind)
-    //addPosition()
-    addPosition('circle',ind)
-    player = 'cross'
-  }else if(player === 'cross' && s.style.backgroundImage ==='') {
-    s.style.backgroundImage = cross;
-    //positions.cross.push(ind)
-    positions[ind]='cross'
-   // addPosition('cross',ind)
-   addPosition('cross',ind)
-    player = 'circle'
+function makeMove(e, ind) {////ставит крестик или нолик
+  const cell = e.target;
+  if(player === 'circle' && cell.style.backgroundImage ==='') {
+    cell.style.backgroundImage = circle;
+    positions[ind]='circle';
+    addPosition('circle');
+    player = 'cross';
+  }else if(player === 'cross' && cell.style.backgroundImage ==='') {
+    cell.style.backgroundImage = cross;
+    positions[ind]='cross';
+    addPosition('cross');
+    player = 'circle';
   }
 };
 
 
 
   itemsList.forEach((item,ind)=>{
-    
-    item.addEventListener('click',(e)=>a(e,ind))
-  })
-
-///выйгрыш
-const win = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
+    item.addEventListener('click',(e)=>makeMove(e,ind));
+  });
