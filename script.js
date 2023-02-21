74b698354eb3f2eca0400ef0bf1f85d8966df0d0
Nidoveralis <nidoveralis@gmail.com>
2023-02-21 21:14:18 +0400

    feat: game with computer

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-const itemsList =document.querySelectorAll('.tic__item');
+const itemsList = document.querySelectorAll('.tic__item');
+const modeGame = document.querySelector('.tic__select');
 const circle = "url('./images/circl.png')";
 const cross = "url('./images/cross.png')";
-let player = 'cross';
-const positions = {};
-const x=[0,1,2];
+const positions = new Object();
 
 function clearField() {
   for(let key in positions) {
     delete positions[key];
   };
-  player = 'cross';
   itemsList.forEach((item)=>{
     item.classList.remove('winner');
     item.style.backgroundImage=null;
@@ -19,20 +17,21 @@ function clearField() {
 function openPopup(win) {
   const popup = confirm(`Победил(а) ${win}. Хотите сыграть ещё раз?`);
   clearField();
+  clickCell();
 };
 
 function winnerShow(ar, win) {
   setTimeout(openPopup, 500, win);
   ar.forEach(elem=>{
     itemsList[elem].classList.add('winner');
-  })
+  });
 };
 
 function addPosition(user) {//проверяет кто выйграл
     if(positions[0]===user && positions[1]===user && positions[2]===user){
       winnerShow([0,1,2], user)
     }else if(positions[3]===user && positions[4]===user && positions[5]===user){
-     winnerShow([3,4,5], win)
+     winnerShow([3,4,5], user)
     }else if(positions[6]===user && positions[7]===user && positions[8]===user){
       winnerShow([6,7,8], user)
     }else if(positions[0]===user && positions[3]===user && positions[6]===user){
@@ -45,30 +44,46 @@ function addPosition(user) {//проверяет кто выйграл
       winnerShow([0,4,8], user)
     }else if(positions[2]===user && positions[4]===user && positions[6]===user){
       winnerShow([2,4,6], user)
-    }else {
-      if(Object.keys(positions).length == 9){
-        setTimeout(openPopup, 500, 'дружба');
-      };
     };
 };
 
-function makeMove(e, ind) {////ставит крестик или нолик
+function searchFreeCells() {//определяет наличие свободных ячеек
+  const freeCells=[];
+  for(i in positions) {
+    if(positions[i]===''){
+      freeCells.push(i);
+    };
+  };
+  if(freeCells.length === 0) {
+    setTimeout(openPopup, 500, 'дружба');
+  } else {
+  moveComp(freeCells);
+  };
+};
+
+function moveComp(move) {///ход компьютера
+  const numberCell = Math.floor(Math.random() * move.length)
+  itemsList[move[numberCell]].style.backgroundImage = circle;
+  positions[move[numberCell]]='circle';
+  addPosition('circle');
+};
+
+function moveUser(e, ind) {////ход игрока
   const cell = e.target;
-  if(player === 'circle' && cell.style.backgroundImage ==='') {
-    cell.style.backgroundImage = circle;
-    positions[ind]='circle';
-    addPosition('circle');
-    player = 'cross';
-  }else if(player === 'cross' && cell.style.backgroundImage ==='') {
+  if(cell.style.backgroundImage ==='') {
     cell.style.backgroundImage = cross;
     positions[ind]='cross';
     addPosition('cross');
-    player = 'circle';
+    searchFreeCells();
   }
 };
 
-
-
+function clickCell() {
   itemsList.forEach((item,ind)=>{
-    item.addEventListener('click',(e)=>makeMove(e,ind));
+  positions[ind]=''
+  item.addEventListener('click',(e)=>{
+    moveUser(e,ind)});
   });
+};
+
+clickCell();
